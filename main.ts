f9a8718b85cbbad620387fe2dd63066d04966ac1
youpy <9128+youpy@users.noreply.github.com>
2023-11-25 20:28:07 +0900

    format typescript with deno fmt

--- main.ts
+++ main.ts
@@ -1,23 +1,23 @@
-import { Application, Router } from "https://deno.land/x/oak@v12.6.1/mod.ts";
-import { bundle } from "https://deno.land/x/emit@0.31.4/mod.ts";
-import { load } from "https://deno.land/x/eszip@v0.55.4/loader.ts";
+import { Application, Router } from 'https://deno.land/x/oak@v12.6.1/mod.ts'
+import { bundle } from 'https://deno.land/x/emit@0.31.4/mod.ts'
+import { load } from 'https://deno.land/x/eszip@v0.55.4/loader.ts'
 
-const app = new Application();
-const router = new Router();
+const app = new Application()
+const router = new Router()
 
 router.get(`/x/:mod(.*@.*)`, async (ctx) => {
-  const mod = ctx.params.mod;
-  const url = new URL("https://deno.land/x/" + mod);
-  const { code } = await bundle(url, { load });
+  const mod = ctx.params.mod
+  const url = new URL('https://deno.land/x/' + mod)
+  const { code } = await bundle(url, { load })
 
   ctx.response.headers.append(
-    "Content-Type",
-    "application/javascript; charset=utf-8"
-  );
-  ctx.response.headers.append("Access-Control-Allow-Origin", "*");
-  ctx.response.body = code;
-});
+    'Content-Type',
+    'application/javascript; charset=utf-8',
+  )
+  ctx.response.headers.append('Access-Control-Allow-Origin', '*')
+  ctx.response.body = code
+})
 
-app.use(router.routes());
+app.use(router.routes())
 
-await app.listen({ port: 8000 });
+await app.listen({ port: 8000 })
